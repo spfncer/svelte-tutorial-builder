9f969576f40bba3e0e5befc3f4c5a01ef7a20831
sfasulo <spencerf0123@gmail.com>
2024-06-11 14:15:55 -0400

    Fix ComputeLocation bug preventing box from being placed underneath

--- src/lib/ComputeLocation.ts
+++ src/lib/ComputeLocation.ts
@@ -1,7 +1,7 @@
 export default function ComputeLocation(item: HTMLElement, box: HTMLDivElement) {
     let x = 0;
     let y = 0;
-    
+
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
@@ -21,13 +21,12 @@ export default function ComputeLocation(item: HTMLElement, box: HTMLDivElement) 
         &&
         itemHoriCenter + boxWidth < windowWidth //does not exceed right edge
         &&
-        itemHoriCenter - boxWidth > 0 //item does not exceed left edge
-    )
-    {
+        itemHoriCenter - (boxWidth / 2) > 0 //item does not exceed left edge
+    ) {
         x = itemHoriCenter - (boxWidth / 2);
         y = itemOffsetBottom + 30;
     }
-        
+
     //test 2: place box to right of item    
     else if (
         itemOffsetRight + boxWidth + 50 < windowWidth //does not exceed right edge
@@ -35,10 +34,9 @@ export default function ComputeLocation(item: HTMLElement, box: HTMLDivElement) 
         itemVertCenter + box.offsetHeight < windowHeight //not exceed bottom edge
         &&
         itemVertCenter - (box.offsetHeight / 2) > 0 //does not exceed top edge
-    )
-    {
-            x = itemOffsetRight + 30;
-            y = itemVertCenter - (boxHeight / 2);
+    ) {
+        x = itemOffsetRight + 30;
+        y = itemVertCenter - (boxHeight / 2);
     }
 
     //test 3: place box to left of item
@@ -48,20 +46,18 @@ export default function ComputeLocation(item: HTMLElement, box: HTMLDivElement) 
         itemVertCenter + box.offsetHeight < windowHeight //does not exceed bottom edge
         &&
         itemVertCenter - (box.offsetHeight / 2) > 0 //does not exceed top edge
-    )
-    {
-        x = item.offsetLeft - boxWidth -  30;
+    ) {
+        x = item.offsetLeft - boxWidth - 30;
         y = itemVertCenter - (boxHeight / 2);
     }
 
     //fallback: place box above item
-    else
-    {
+    else {
         x = itemHoriCenter - (boxWidth / 2);
         y = item.offsetTop - 30;
     }
 
-    return{
+    return {
         'x': x.toString(),
         'y': y.toString()
     }
